405263c6b74d42a2e5e035d64357184d7e1cc615
Paul-Aguilar <ie709867@iteso.mx>
2021-02-10 20:54:47 -0600

    sw3 sirve, delay muy rapido aun

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 #include <stdint.h>
 #include "MK64F12.h"
 //#define DEBUG_ON
-int main(void) {
+int main(void)
+	{
 	/**Activating the GPIOB clock gating*/
 	SIM->SCGC5 |= 0x2E00;
 	SIM->SCGC5 |= 0x0200;
@@ -29,18 +30,21 @@ int main(void) {
 	uint16_t sw3_value = 0;
 	uint16_t time_delay = 6500;
 
-	while(1) {
-
+	while(1)
+	{
 		sw2_value = GPIOC->PDIR;
 		sw3_value = GPIOA->PDIR;
 
-		uint16_t time_delay = 6500;
+		sw2_value = sw2_value & 0x40;
+		sw3_value = sw3_value & 0x10;
+
+
 
 		if (0 != sw2_value)
 		{
 			if(0 != sw3_value)
 			{
-				uint16_t time_delay = 6500;
+				uint32_t time_delay = 6500000;
 
 				GPIOB->PCOR = 0x00400000;
 				delay(time_delay);
@@ -53,12 +57,10 @@ int main(void) {
 				GPIOE->PCOR = 0x04000000;
 				delay(time_delay);
 				GPIOE->PSOR = 0x04000000;
-
-
 			}
 			if(0 == sw3_value)
 			{
-				uint16_t time_delay = 6500*3;
+				uint32_t time_delay = 6500000/3;
 
 				GPIOB->PCOR = 0x00400000;
 				delay(time_delay);
@@ -72,30 +74,25 @@ int main(void) {
 				delay(time_delay);
 				GPIOE->PSOR = 0x04000000;
 			}
-
 		}
 		if (0 == sw2_value)
 		{
 			if(0 != sw3_value)
 			{
-				uint16_t time_delay = 6500;
+				uint32_t time_delay = 6500000;
 				white_on();
 				delay(time_delay);
 				white_off();
 			}
 			if(0 == sw3_value)
 			{
-				uint16_t time_delay = 6500*3;
+				uint32_t time_delay = 6500000*3;
 				white_on();
 				delay(time_delay);
 				white_off();
 			}
 		}
-
-
-
-
-		}
+	}
 	return 0 ;
 }
 
